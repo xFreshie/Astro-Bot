e6691f7f96147ded982ffb40a3ba61b4b1c30967
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-07-25 15:07:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -169,6 +169,48 @@ client.on("message", async message => {
     }, ms(mutetime));
 
 }
+if(command === "pages") {
+  let pages = ['This is page one!', 'Second page', 'Third', 'You can add pages', 'All you need to do is add another item in the array', '**Supports markdown and regular chat description properties**']; 
+  let page = 1; 
+ 
+  const embed = new Discord.MessageEmbed() 
+    .setColor(0xffffff)
+    .setFooter(`Page ${page} of ${pages.length}`) 
+    .setDescription(pages[page-1])
+ 
+  message.channel.send(embed).then(msg => { 
+   
+    msg.react('⏪').then( r => { 
+      msg.react('⏩') 
+     
+      const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+      const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
+     
+      const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+      const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
+     
+      
+      backwards.on('collect', r => { 
+        if (page === 1) return; 
+        page--; 
+        embed.setDescription(pages[page-1]); 
+        embed.setFooter(`Page ${page} of ${pages.length}`); 
+        msg.edit(embed) 
+      })
+     
+      forwards.on('collect', r => { 
+        if (page === pages.length) return; 
+        page++; 
+        embed.setDescription(pages[page-1]); 
+        embed.setFooter(`Page ${page} of ${pages.length}`); 
+        msg.edit(embed) 
+      })
+   
+    })
+ 
+  })
+ 
+}
 const yourID = "427858680550260736";
 const setupCMD = "a!reactroles"
 let initialMessage = `**React to the messages below to receive the verified role, this is in case of spam bots or stuff. If you would like to chat in other channels, simply react to the message.**`;
