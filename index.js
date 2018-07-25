3d91bbdbb259d101dbd9c9bea800693f96130e6d
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-07-25 15:18:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -229,6 +229,19 @@ if(command === "lockdown") {
     });
   }
 };
+if(command === "warn") {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, but you don't have permission to use this!") 
+   let warnedmember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if (!warnedmember) return ("Please mention a user to warn.");
+     let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+   
+    
+      message.delete().catch(O_o=>{});
+    message.channel.send(`***${warnedmember.user.tag} was warned!***`)
+   await warnedmember.send(`You have been warned in ${message.guild.name} by ${message.author.username} for: ${reason}.`)
+  
+  }
 const yourID = "427858680550260736";
 const setupCMD = "a!reactroles"
 let initialMessage = `**React to the messages below to receive the verified role, this is in case of spam bots or stuff. If you would like to chat in other channels, simply react to the message.**`;
