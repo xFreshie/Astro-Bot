a1240489e36273d4eb4ec1a70484c698800766c2
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-07-25 15:16:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -194,50 +194,41 @@ if(command === "serverinfo") {
 
 }
 if(command === "lockdown") {
-if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('Sorry, you don\'t have permission to lockdown or unlock!')
-    .then(msg => msg.delete({
-        timeout: 10000
-    }));
-if (!client.lockit) client.lockit = [];
-let time = args.join(' ');
-let validUnlocks = ['release', 'unlock'];
-if (!time) return message.channel.send('You must set a duration for the lockdown in either hour(s), minute(s) or second(s)');
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You can't use that command.");
+  if (!client.lockit) client.lockit = [];
+  let time = args.join(' ');
+  let validUnlocks = ['release', 'unlock'];
+  if (!time) return message.reply('You must set a duration for the lockdown in either hours, minutes or seconds');
 
-if (validUnlocks.includes(time)) {
-    message.channel.updateOverwrite(message.guild.id, {
+  if (validUnlocks.includes(time)) {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: null
+    }).then(() => {
+      message.channel.sendMessage('Lockdown lifted.');
+      clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
+    }).catch(error => {
+      console.log(error);
+    });
+  } else {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: false
+    }).then(() => {
+      message.channel.sendMessage(`Channel locked down for ${ms(ms(time), { long:true })}`).then(() => {
+
+        client.lockit[message.channel.id] = setTimeout(() => {
+          message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: null
-        })
-        .then(() => {
-            message.channel.send('Lockdown lifted.');
-            clearTimeout(client.lockit[message.channel.id]);
-            delete client.lockit[message.channel.id];
-        })
-        .catch(error => {
-            console.log(error);
-        });
-} else {
-    message.channel.updateOverwrite(message.guild.id, {
-            SEND_MESSAGES: false
-        })
-        .then(() => {
-            message.channel.send(`Channel locked down for ${ms(ms(time), { long:true })}`)
-                .then(() => {
+          }).then(message.channel.sendMessage('Lockdown lifted.')).catch(console.error);
+          delete client.lockit[message.channel.id];
+        }, ms(time));
 
-                    client.lockit[message.channel.id] = setTimeout(() => {
-                        message.channel.updateOverwrite(message.guild.id, {
-                                SEND_MESSAGES: null
-                            })
-                            .then(message.channel.send('Lockdown lifted.'))
-                            .catch(console.error);
-                        delete client.lockit[message.channel.id];
-                    }, ms(time));
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        });
-}
-}
+      }).catch(error => {
+        console.log(error);
+      });
+    });
+  }
+};
 const yourID = "427858680550260736";
 const setupCMD = "a!reactroles"
 let initialMessage = `**React to the messages below to receive the verified role, this is in case of spam bots or stuff. If you would like to chat in other channels, simply react to the message.**`;
